475957fc1e04f84d7b4992d33bb8cda20a5397eb
Fabiano Nalin <fnalin@msn.com>
2015-10-15 23:45:21 -0300

    01. Comparando via TPL com Async
    Comparação de projetos simples

---
+++ 01. Comparando via TPL com Async/1. DemoTPLSimples/DemoTPLSimples/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DemoTPLSimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var result = SlowOperation();
+            //inicia em uma nova thread o método e continua na atual
+            var task = Task.Factory.StartNew<int>(SlowOperation);
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            //Console.WriteLine("Slow operation result: {0}", result);
+            //devido ao task.result, ele aguarda aqui p resultado da thread
+            Console.WriteLine("Slow operation result: {0}", task.Result);
+            Console.WriteLine("Main complete on {0}", 
+                Thread.CurrentThread.ManagedThreadId);
+
+            Console.ReadLine();
+
+        }
+
+        private static int SlowOperation()
+        {
+            Console.WriteLine("Slow Operation started on {0}",
+                Thread.CurrentThread.ManagedThreadId);
+
+            Thread.Sleep(3000);
+
+            Console.WriteLine("Slow operation complete on {0}",
+                Thread.CurrentThread.ManagedThreadId);
+
+            return 10;
+        }
+    }
+}
---
+++ 01. Comparando via TPL com Async/2. DemoAsyncSimples/DemoAsyncSimples/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DemoAsyncSimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //chama o método usando a mesma thread
+            var task = SlowOperation();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            //Console.WriteLine("Slow operation result: {0}", result);
+            //aguarda aqui o resultado da outra thread
+            Console.WriteLine("Slow operation result: {0}", task.Result);
+            Console.WriteLine("Main complete on {0}",
+                Thread.CurrentThread.ManagedThreadId);
+
+            Console.ReadLine();
+        }
+
+        private static async Task<int> SlowOperation()
+        {
+            Console.WriteLine("Slow Operation started on {0}",
+                Thread.CurrentThread.ManagedThreadId);
+
+            //inicia uma nova thread aqui e continua
+            await Task.Delay(3000);
+
+            Console.WriteLine("Slow operation complete on {0}",
+                Thread.CurrentThread.ManagedThreadId);
+
+            return 10;
+        }
+    }
+}
