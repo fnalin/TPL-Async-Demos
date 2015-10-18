682f8f49e5bedcd1462fc40de66a8937009bf0c5
Fabiano Nalin <fnalin@msn.com>
2015-10-18 13:36:46 -0200

    Add Demo 02

---
+++ 02. Exemplo Async simples com log na tela/DemoAsyncComLog/Async.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DemoAsyncComLog
+{
+    class Async
+    {
+
+
+        static void Main()
+        {
+
+            Console.WriteLine("--> início do método Main -  ID da Thread atual: {0}", Thread.CurrentThread.ManagedThreadId);
+
+            var task = OperacaoAsync();
+
+            Console.WriteLine("\n--> estou no método Main antes do for -  ID da Thread atual: {0}\n", Thread.CurrentThread.ManagedThreadId);
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("... " + i);
+            }
+
+
+            Console.WriteLine("\n\n--> estou no método Main após o for - ID da Thread atual: {0}",  Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine("\nResposta da operação: " + task.Result);
+            Console.WriteLine("\n--> estou no método Main após o resultado da task - ID da Thread atual: {0}", Thread.CurrentThread.ManagedThreadId);
+
+            Console.WriteLine("\nFim, pressione Enter");
+            Console.ReadLine();
+        }
+
+        private static async Task<int> OperacaoAsync()
+        {
+            Console.WriteLine("\n--> início da operacaoasync - ID da Thread atual: {0}", Thread.CurrentThread.ManagedThreadId);
+            //await Task.Delay(2000);
+
+            await Task.Run(() =>
+            {
+                Thread.Sleep(3000);
+                Console.WriteLine("\n--> dentro da operacaoasync em task.run - ID da Thread atual: {0}", Thread.CurrentThread.ManagedThreadId);
+            });
+
+            Console.WriteLine("\n--> fim da operacaoasync antes do return - ID da Thread atual: {0}", Thread.CurrentThread.ManagedThreadId);
+            return 42;
+        }
+    }
+}
---
+++ 02. Exemplo Async simples com log na tela/DemoAsyncComLog/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DemoAsyncComLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iniciando o código...");
+            //var tarefa = Task.Factory.StartNew<int>(OperacaoDemorada);
+            //var tarefa = Task.Factory.StartNew(() => OperacaoDemorada(5));
+
+            var tarefa = Task.Factory.StartNew((object x) =>
+            {
+                Thread.Sleep(2000);
+                return (int)x * 2;
+            }, 5);
+
+
+            Task.Factory.StartNew((object myState) =>
+            {
+                var i = (int)myState;
+
+                //Do calculations...
+                var x = i + 10;
+            }, 10);
+
+
+            Console.WriteLine("Iniciando restando do código");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("Resposta do método {0}", tarefa.Result);
+
+            Console.WriteLine("Fim");
+            Console.ReadLine();
+
+
+        }
+
+        private static int OperacaoDemorada(int x)
+        {
+            Thread.Sleep(3000);
+            return x * 2;
+        }
+    }
+}
